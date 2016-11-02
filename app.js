64db266e27142f64144f7f33fd1062b73c1ac1da
Guillaume Chave <gchave@peaks.fr>
2016-11-02 20:44:59 +0100

    enabled papertail logs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var config = require('./config'),
-        express = require('express'),
-        _ = require('lodash'),
-        bodyParser = require('body-parser'),
-        morgan = require('morgan'),
-        RF = require('rest-framework');
+  express = require('express'),
+  _ = require('lodash'),
+  bodyParser = require('body-parser'),
+  morgan = require('morgan'),
+  RF = require('rest-framework');
 
 var app = exports = module.exports = express();
 
@@ -22,10 +22,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(RF.Cors.middleware);
 app.use(RF.Cors.redirect);
 
-if (config.debug) {
-    app.use(morgan('short'));
-}
-
 app.db = require('./model/model.js')(app, config);
 app.use(function(req, res, next) {
     app.db.generateUrl = function(path) {
@@ -34,7 +30,44 @@ app.use(function(req, res, next) {
     next();
 });
 
+var winston = require('winston');
+require('winston-papertrail').Papertrail;
+
+var winstonPapertrail = new winston.transports.Papertrail({
+    host: process.env.PAPERTRAIL_HOST,
+    port: process.env.PAPERTRAIL_PORT
+});
+
+var logger = new winston.Logger({
+    transports: [winstonPapertrail]
+});
+
+logger.stream = {
+    write: function(message){
+        logger.info(message);
+    }
+};
+
+app.use(morgan('combined', { stream: logger.stream }));
+
 var errorHandler = RF.Error(config);
+errorHandler.formatError = function(statusCode, message, details) {
+    switch (statusCode) {
+        case 404:
+            logger.warn(statusCode, message, details);
+            break;
+        default:
+            logger.error(statusCode, message, details);
+    }
+
+    return {
+        statusCode: statusCode,
+        error: message,
+        details: details,
+        date: new Date()
+    }
+};
+
 app.errorHandler = errorHandler;
 
 var Routing = RF.Routing(app, config.security, {
