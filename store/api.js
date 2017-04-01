a148b0f5cd6b780b9b08e547ceba713ba11ec420
onishinji <onishinji@gmail.com>
2017-04-01 09:21:08 +0200

    disabled cabin and fix stats

--- store/api.js
+++ store/api.js
@@ -4,6 +4,7 @@ var _ = require("lodash");
 var cheerio = require('cheerio');
 var request = require("request");
 var parseString = require('xml2js').parseString;
+var moment = require("moment")
 
 module.exports = function(app, config) {
     return new Store(app, config);
@@ -27,13 +28,12 @@ Store.prototype.getUserById = function(id) {
 
                 $ = cheerio.load(response.body);
 
-                var averageWordPerDay = parseInt($($("#camper_stats dd")[0]).text().replace(",", ""));
-                var wordcountToday = parseInt($($("#camper_stats dd")[2]).text().replace(",", ""));
-                var userWordToReach = parseInt($($("#camper_stats dd")[3]).text().replace(",", ""));
-                var userWordCount = parseInt($($("#camper_stats dd")[4]).text().replace(",", ""));
+                var averageWordPerDay = parseInt($($("#camper_stats dd")[6]).text().replace(",", "")); // Your Average Per Day 6
+                var wordcountToday = parseInt($($("#camper_stats dd")[2]).text().replace(",", "")); // Words Written Today
+                var userWordToReach = parseInt($($("#camper_stats dd")[0]).text().replace(",", ""));
+                var userWordCount = parseInt($($("#camper_stats dd")[3]).text().replace(",", ""));
 
-                var nbDayRemaining = parseInt($($("#camper_stats dd")[7]).text().replace(",", ""));
-                var dailyTarget = parseInt($($("#camper_stats dd")[9]).text().replace(",", ""));
+                var nbDayRemaining = moment().daysInMonth() - moment().date() + 1
 
                 var username = $("p.username a").text();
 
@@ -65,10 +65,10 @@ Store.prototype.getUserById = function(id) {
                     })
                 }
 
-                nbDayRemaining = nbDayRemaining != undefined ? nbDayRemaining : 0;
-                wordcountToday = wordcountToday != undefined ? wordcountToday : 0;
+                nbDayRemaining = nbDayRemaining || 0;
+                wordcountToday = wordcountToday || 0;
                 // 
-                dailyTarget = (userWordToReach - userWordCount + wordcountToday) / nbDayRemaining;
+                var dailyTarget = (userWordToReach - userWordCount + wordcountToday) / nbDayRemaining;
 
                 return resolve({
                     id: id,
@@ -96,6 +96,9 @@ Store.prototype.getCabinByUserId = function(id) {
     return new Promise(function(resolve, reject) {
 
         request.get(self.config.endpoint.replace(":username", id), function(error, response, body) {
+
+            // disabled cabin stats, in camp 04/2017 cabin stats aren't available
+            return reject(new self.app.errorHandler.NotFoundError("", "cabin", id));
 
             if (!error && response.statusCode == 200) {
 
